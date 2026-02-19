4d59bc4dbc6a78f45f556a4be2309f8efa1cb5bb
Volodymyr Tykhoniuk <volodymyr.tykhoniuk@gmail.com>
2026-02-19 15:28:54 +0200

    Decommissing some patches and adding new ones. Twikking config.h accordingly

--- config.h
+++ config.h
@@ -10,14 +10,17 @@ static const unsigned int gappov    = 30;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=20" };
-static const char dmenufont[]       = "monospace:size=20";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#5c2e10";
-//static const char col_cyan[]        = "#d65d0e";
+static const char *fonts[]          = { "xft:sans:size=16" };
+static const char dmenufont[]       = "xft:sans:size=16";
+static unsigned int baralpha        = 0x0;
+static unsigned int borderalpha     = OPAQUE;
+static const char col_gray1[]       = "#282828";
+static const char col_gray2[]       = "#3c3836";
+static const char col_gray3[]       = "#ebdbb2";
+static const char col_gray4[]       = "#282828";
+static const char col_cyan[]        = "#d65d0e";
+//static const char col_cyan[]        = "#c76802";
+//static const char col_cyan[]        = "#fabd2f";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -26,30 +29,35 @@ static const char *colors[][3]      = {
 
 static const char *const autostart[] = {
 //	"xset", "r", "rate", "200", "30", NULL,
+	"dwmblocks", NULL,
 	"picom", NULL,
 	"/bin/sh", "-c", "~/.fehbg", NULL,
+	"chromium", NULL,
 	"signal-desktop", NULL,
 	"Telegram", NULL,
-	"chromium", NULL,
-	NULL /* terminate */
+    "chromium", "--app=https://web.whatsapp.com", NULL,
+    NULL /* terminate */
 };
 
 
 /* tagging */
-static const char *tags[] = { "", "󰭹", "3", "", "" };
+static const char *tags[]       = { "", "󰭹", "", "" };
+static const int taglayouts[]   = {  0,   7,   0,   13 };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class            instance    title   tags mask   isfloating  CenterThisWindow?   monitor border width*/
-	{ "st",             NULL,       NULL,   0,          0,          1,                 -1,     -1 },
-	{ "steam",          NULL,       NULL,   1<<4,       1,          0,                 -1,      0 },
-	{ "Piper",          NULL,       NULL,   0,          1,          0,                 -1,     -1 },
-	{ "Chromium",       NULL,       NULL,   1<<3,       0,          0,                 -1,      0 },
-	{ "Signal",         NULL,       NULL,   1<<1,       1,          0,                 -1,     -1 },
-	{ "TelegramDesktop",NULL,       NULL,   1<<1,       1,          0,                 -1,      -1 },
+	/* class            instance            title   tags mask   isfloating  monitor border width*/
+	{ "st",             NULL,               NULL,   0,          0,          -1,     -1 },
+	{ "steam",          NULL,               NULL,   1<<3,       1,          -1,      0 },
+	{ "Piper",          NULL,               NULL,   0,          1,          -1,     -1 },
+	{ "vlc",            NULL,               NULL,   0,          1,          -1,     -1 },
+	{ "Chromium",       "chromium",         NULL,   1<<2,       0,          -1,      0 },
+	{ "Signal",         NULL,               NULL,   1<<1,       0,          -1,     -1 },
+	{ "TelegramDesktop",NULL,               NULL,   1<<1,       0,          -1,     -1 },
+	{ "Chromium",       "web.whatsapp.com", NULL,   1<<1,       0,          -1,     -1 },
 };
 
 /* layout(s) */
@@ -57,27 +65,27 @@ static const float mfact        = 0.5;  /* factor of master area size [0.05..0.9
 static const int nmaster        = 1;    /* number of clients in master area */
 static const int resizehints    = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1;    /* 1 will force focus on the fullscreen window */
-static const int refreshrate    = 120;  /* refresh rate (per second) for client move/resize */
+static const int refreshrate    = 240;  /* refresh rate (per second) for client move/resize */
 
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
 #include "vanitygaps.c"
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },				/* 0 first entry is default */
+	{ "󰕴",        tile },				/* 0 first entry is default */
 	{ "[M]",      monocle },			/* 1 */
 	{ "[@]",      spiral },				/* 2 */
 	{ "[\\]",     dwindle },			/* 3 */
 	{ "H[]",      deck },				/* 4 */
 	{ "TTT",      bstack },				/* 5 */
-	{ "===",      bstackhoriz },			/* 6 */
-	{ "HHH",      grid },				/* 7 */
+	{ "󰕵",        bstackhoriz },			/* 6 */
+	{ "󰕳",        grid },				/* 7 */
 	{ "###",      nrowgrid },			/* 8 */
 	{ "---",      horizgrid },			/* 9 */
 	{ ":::",      gaplessgrid },			/* 10 */
 	{ "|M|",      centeredmaster },			/* 11 */
 	{ ">M>",      centeredfloatingmaster },		/* 12 */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "󰒅",        NULL },    /* no layout function means floating behavior */
 	{ NULL,       NULL },
 };
 
@@ -92,34 +100,44 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
+#define STATUSBAR "dwmblocks"
+
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", "-c", "st", NULL };
+//static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "ghostty", "+new-window", NULL };
 static const char *chromiumcmd[]  = { "chromium", NULL };
-static const char *incvol[] = {"/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+2%", NULL};
-static const char *decvol[] = {"/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "-2%", NULL};
+static const char *incvol[] = {"/usr/bin/wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@", "0.01+", NULL};
+static const char *decvol[] = {"/usr/bin/wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@", "0.01-", NULL};
+
+static const char scratchpadname[] = "scratchpad";
+//static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, NULL };
+static const char *scratchpadcmd[] = { "ghostty", "--title=scratchpad", "--quit-after-last-window-closed=true", NULL };
+
 
 #include "movestack.c"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 //	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_b,      spawn,          {.v = chromiumcmd } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-//	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
-//	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = -0.25} },
-	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = +0.25} },
-//	{ MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} },
+	{ MODKEY,                       XK_minus,  setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_equal,  setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_minus,  setcfact,       {.f = -0.25} },
+	{ MODKEY|ShiftMask,             XK_equal,  setcfact,       {.f = +0.25} },
+	{ MODKEY|ShiftMask,             XK_0,      setcfact,       {.f =  0.00} },
 	{ MODKEY,                       XK_Tab,    zoom,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
+    { MODKEY,                       XK_x,      killclient,     {.ui = 1} },  // kill unselect
 	{ MODKEY|Mod1Mask,              XK_u,      incrgaps,       {.i = +1 } },
 	{ MODKEY|Mod1Mask|ShiftMask,    XK_u,      incrgaps,       {.i = -1 } },
 	{ MODKEY|Mod1Mask,              XK_i,      incrigaps,      {.i = +1 } },
@@ -140,6 +158,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_z,      setlayout,      {.v = &layouts[6]} },
+	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[7]} },
 	{ MODKEY,                       XK_f,      togglefloating, {0} },
     /*
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
@@ -163,22 +182,23 @@ static const Key keys[] = {
 	{ 0,				XF86XK_AudioRaiseVolume,spawn,{.v = incvol} },
 	{ MODKEY|ShiftMask,             XK_q,   quit,           {1} },
 	{ MODKEY|ControlMask,           XK_q,   quit,           {0} }, 
-    { MODKEY,                       XK_s,   scratchpad_show, {.i = 1} },
-    { MODKEY,                       XK_y,   scratchpad_show, {.i = 2} },
-//    { MODKEY,                       XK_u,   scratchpad_show, {.i = 3} },
-    { MODKEY|ShiftMask,             XK_s,   scratchpad_hide, {.i = 1} },
-    { MODKEY|ShiftMask,             XK_y,   scratchpad_hide, {.i = 2} },
-//    { MODKEY|ShiftMask,             XK_u,   scratchpad_hide, {.i = 3} },
-    { MODKEY|ShiftMask,             XK_r,   scratchpad_remove,           {0} },
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+//	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+//	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkStatusText,        0,              Button1,        sigstatusbar,   {.i = 1} },
+	{ ClkStatusText,        0,              Button2,        sigstatusbar,   {.i = 2} },
+	{ ClkStatusText,        0,              Button3,        sigstatusbar,   {.i = 3} },
+	{ ClkStatusText,        0,              Button4,        sigstatusbar,   {.i = 4} },
+	{ ClkStatusText,        0,              Button5,        sigstatusbar,   {.i = 5} },
+	{ ClkStatusText,        0,              6,              sigstatusbar,   {.i = 6} },
+	{ ClkStatusText,        0,              7,              sigstatusbar,   {.i = 7} },
+	{ ClkStatusText,        0,              8,              sigstatusbar,   {.i = 8} },
+	{ ClkStatusText,        0,              9,              sigstatusbar,   {.i = 9} },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
